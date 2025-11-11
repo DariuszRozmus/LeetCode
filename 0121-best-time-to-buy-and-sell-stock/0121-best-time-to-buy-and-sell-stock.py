85f37ac280eea707f4654d727d4629bba22de1f7
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2025-11-11 10:02:18 +0100

    Time: 115 ms (36.88%), Space: 20.7 MB (11.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -5,16 +5,18 @@ class Solution(object):
         :rtype: int
         """
         n = len(prices)
-        result = 0
-        T_min = [prices[0]]*n
+    
+        tmin = prices[0]
         T_max = [prices[n-1]]*n
-        for i in range(1,n):
-            T_min[i] = min(T_min[i-1], prices[i])
-        for i in range(n-2,0,-1):
+        
+        for i in range(n-2,-1,-1):
             T_max[i] = max(T_max[i+1],prices[i])
-        # print(T_min)
         # print(T_max)
-        result = 0
-        for i in range(n):
-            result = max(T_max[i]-T_min[i],result)
+        
+        result = max(0, T_max[0]-tmin)
+
+        for i in range(1,n):
+            if(tmin > prices[i]):
+                tmin = prices[i]
+                result = max(result, T_max[i]-tmin)
         return result
