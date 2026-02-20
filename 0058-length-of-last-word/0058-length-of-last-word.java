3ecf483748aac6cb4415eb9a2bcbb11de80d15df
Dariusz Rozmus <drozmus@student.agh.edu.pl>
2026-02-20 10:34:56 +0100

    Time: 1 ms (33.38%), Space: 43.2 MB (51.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,12 +3,7 @@ class Solution {
         String[] a = s.split(" ");
         // System.out.println(a.length);
         int cnt = 0; 
-        for (String str : a){
-            if(str.length() > cnt){
-                cnt = a[a.length -1].length();
-            }
-            
-        }
+        cnt = a[a.length -1].length();
         return cnt;
     }
 }
